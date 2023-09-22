63ea38601df6c1640aeb4e36116d84143ef59134
yuyeoul <yyb9719@gmail.com>
2023-09-22 10:34:26 +0900

    Chore: N-Queen_YuYeoul

--- 08_Week9/YuYeoul/BOJ_9663.java
+++ 08_Week9/YuYeoul/BOJ_9663.java
@@ -1,3 +1,5 @@
+package BOJ_9663_NQueen;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,15 +8,18 @@ public class Main {
 	static int cnt=0;
 	static int arr[];
 	public static void main(String[] args) {
+		//체스판 생성 후 DFS호출
 		N=sc.nextInt();
 		arr=new int [N];
 		DFS(0);
-		System.out.println(cnt);
 	}
+	//DFS파트
+	//백트래킹 파트에서 true받으면 재귀 호출
 	static void DFS(int d) {
 		if(d==N) {
-			cnt++;
-			return ;
+			for(int i=0;i<N;i++) {
+				System.out.println(arr[i]);
+			}
 		}
 		for(int i=0;i<N;i++) {
 			arr[d]=i;
@@ -24,10 +29,13 @@ public class Main {
 		}
 		
 	}
+	//백트래킹 파트
 	static boolean search(int c) {
 		for(int i=0;i<c;i++) {
+			//같은 라인에 퀸이 있다면 false
 			if(arr[c]==arr[i]) {
 				return false;
+			//대각선에 퀸이 있다면 false
 			}else if(c-i==Math.abs(arr[c]-arr[i]) ) {
 				return false;
 			}
